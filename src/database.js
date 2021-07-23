f310a16a23ab64f20c4fa65efe6941ba70566704
CharlesProm <62314701+CharlesProm@users.noreply.github.com>
2021-07-23 16:50:20 -0400

    cleaning

--- src/database.js
+++ src/database.js
@@ -1,19 +1,6 @@
-
-// const proConfig = {
-//     connectionString: process.env.DATABASE_URL
-// }
-// const proConfig2 = {
-//     connectionString: "postgres://yibanthznkhlhj:29a74f842838dfb4d555654ce4549e2b80374830837d664c6719d8d5862acae5@ec2-52-6-77-239.compute-1.amazonaws.com:5432/dd9o2bt2jre6rm"
-// }
-// const config = {
-//     user: 'postgres',
-//     host: 'localhost',
-//     password: '123123',
-//     database: 'my_first_DB'
-// }
 const { Pool } = require('pg')
 require('dotenv').config()
-const config2 = {
+const config = {
     user: 'yibanthznkhlhj',
     host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
     password: "29a74f842838dfb4d555654ce4549e2b80374830837d664c6719d8d5862acae5",
@@ -23,7 +10,6 @@ const config2 = {
     rejectUnauthorized: false,
   },
 }
-
-const pool = new Pool(config2)
+const pool = new Pool(config)
 
 module.exports = pool
